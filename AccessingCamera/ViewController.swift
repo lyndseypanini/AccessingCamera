aff21260d6716297a8ba652500ccf75dfc1e3ea0
Lyndsey Narvaez <lyndseypanini@gmail.com>
2020-07-14 11:43:47 -0700

    Initial Commit

---
+++ AccessingCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AccessingCamera
+//
+//  Created by Lyndsey Narvaez on 7/14/20.
+//  Copyright © 2020 Lyndsey Narvaez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
